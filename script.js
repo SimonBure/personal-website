cf47aca5d7ddc0a374a7ec98fd057a511248e2b0
simon <simon.bure@mailo.com>
2025-04-02 11:53:22 +0200

    Ajout des cartes interactives pour détailler les projets, écrits et publications. Changement du footer, ajout des liens github et linkedin sur la page d'acceuil

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal(title, description) {
+    document.getElementById('modal-title').innerText = title;
+    document.getElementById('modal-description').innerText = description;
+    document.getElementById('modal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
